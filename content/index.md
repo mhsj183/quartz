c0bda3531adb9794c34f4472a3caefc0a998bb87
mhsj <mhsj@mhsjdeMacBook-Air.local>
2026-02-07 20:09:38 +0800

    首次尝试提交

---
+++ content/index.md
@@ -0,0 +1,6 @@
+---
+title: Welcome to Quartz
+tags: happy
+---
+
+很高兴认识你。
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
-    pageTitleSuffix: "",
+    pageTitle: "mhsj",
+    pageTitleSuffix: " - mhsj",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "zh-CN",
+    baseUrl: "quartz.mhsj.live",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
